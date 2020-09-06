ac6c1edaf20fe4303119d879c006fd854a9b99c5
JeonghakHeo <68264328+JeonghakHeo@users.noreply.github.com>
2020-09-06 19:34:56 +1000

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link, NavLink } from 'react-router-dom';
 
+// npm install react-router-dom
 const Navbar = () => {
   return(
     <nav className="nav-wrapper red darken-3">
