0e73904429786865e56ca1038027df04eb1b2330
Charlie <huh4142@naver.com>
2020-09-11 20:20:53 +1000

    lesson-40 Setting up Redux in React

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux' 
 // Provider allows react application to interact with redux
 // Provider wraps our root <App /> component providing it with store
 import rootReducer from './reducers/rootReducer';
--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,13 +1,9 @@
 const initState = {
-  posts: [
-    {id: '1', title: 'Squirtle laid an Egg', body:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus incidunt expedita tempora laboriosam quae aliquam omnis aliquid error repudiandae ad itaque corporis tempore suscipit, pariatur dolorum commodi, recusandae veritatis dolor?'},
-    {id: '2', title: 'Charmander Laid an Egg', body:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus incidunt expedita tempora laboriosam quae aliquam omnis aliquid error repudiandae ad itaque corporis tempore suscipit, pariatur dolorum commodi, recusandae veritatis dolor?'},
-    {id: '3', title: 'a Helix Fossil was Found', body:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus incidunt expedita tempora laboriosam quae aliquam omnis aliquid error repudiandae ad itaque corporis tempore suscipit, pariatur dolorum commodi, recusandae veritatis dolor?'}
-  ]
+  posts: []
 }
 
 const rootReducer = (state = initState, action) => {
   return state;
 }
 
-export default rootReducer
+export default rootReducer;
